bf6161cda3e8d3869bce748465e91dc5ab85051d
furuya <furuya003@gmail.com>
2022-03-06 13:25:06 +0900

    feat: Writing markup with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,20 @@ export default function App(): JSX.Element {
     <div>
       <h1>Welcome to my App</h1>
       <MyButton />
+      <AboutPage />
     </div>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        Hello there.
+        <br />
+        How do you do?
+      </p>
+    </>
   );
 }
