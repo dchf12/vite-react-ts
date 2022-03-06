3429a987318010ce47de8c05b210a93f67cbc2e4
furuya <furuya003@gmail.com>
2022-03-06 14:07:45 +0900

    feat: Rendering lists

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ export default function App(): JSX.Element {
       <MyButton />
       <AboutPage />
       <Profile />
+      <ShopList />
     </div>
   );
 }
@@ -49,10 +50,23 @@ function Profile() {
   );
 }
 
-let content;
-if (isLoggedIn) {
-  content = <AdminPanel />;
-} else {
-  content = <LoginForm />;
+const products = [
+  { title: "Cabbage", isFruit: false, id: 1 },
+  { title: "Garlic", isFruit: false, id: 2 },
+  { title: "Apple", isFruit: true, id: 3 },
+];
+
+function ShopList() {
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? "red" : "green",
+      }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
 }
-return <div>{content}</div>;
