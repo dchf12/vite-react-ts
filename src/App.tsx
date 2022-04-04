23d1090412bc6fdb15e3ab3aacd4baceaff40a16
furuya <furuya003@gmail.com>
2022-04-04 20:16:23 +0900

    feat: Add stopwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useRef } from 'react';
 
 function MyButton({ count, onClick }: { count: number; onClick: () => void }): JSX.Element {
   return <button onClick={onClick}>Clicked {count} times</button>;
@@ -19,6 +20,7 @@ export default function App(): JSX.Element {
       <AboutPage />
       <Profile />
       <ShopList />
+      <StopWatch />
     </div>
   );
 }
@@ -88,4 +90,33 @@ function ShopList(): JSX.Element {
   ));
 
   return <ul>{listItems}</ul>;
+}
+
+function StopWatch(): JSX.Element {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
+  const intervalRef = useRef<number>();
+
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+    clearInterval(intervalRef.current);
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+  function handleStop() {
+    clearInterval(intervalRef.current);
+  }
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+  return (
+    <>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </>
+  );
 }
